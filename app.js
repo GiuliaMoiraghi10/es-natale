388fa6c71db8c73c3406effdc18a473ea36c5eb5
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-31 17:11:49 +0100

    Gestione errore + middleware

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// MIDDLEWARES
 // cors --> verifica che la richiesta sia autorizzata prima del trasferimento dati
 const cors = require('cors')
+const errorsHandler = require('./middlewares/errorHandler')
+const notFound = require('./middlewares/notFound')
+
+// ROUTERS
+const characterRouter = require('./routers/characterRouter')
+
+// Utilizzo i Middlewares --> gestione errore
+app.use(cors())
+app.use(errorsHandler)
+app.use(notFound)
+
+// accetto file json nella body req
+app.use(express.json())
 
 // prima porta
 app.get('/', (req, res) => {
     res.send('Prima porta')
 })
+
+app.use('/characters', characterRouter)
 
 // ascolto porta
 app.listen(port, () => {
